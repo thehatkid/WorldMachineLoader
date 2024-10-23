65195328cebb72c8b2d5681bdc3836577fc028da
hat_kid <saha22023@gmail.com>
2024-10-24 01:52:41 +0700

    Add sample mod project

---
+++ SampleMods/SampleMod/Mod.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using OneShotMG.src;
+
+namespace SampleMod
+{
+    internal class Mod
+    {
+    }
+
+    [HarmonyPatch(typeof(LanguageManager))]
+    class WME_LanguageManagerPatch
+    {
+        /// <summary>Replaces any localized strings with our "Shoutouts to Simpleflips" one.</summary>
+        [HarmonyPrefix]
+        [HarmonyPatch("GetLocString")]
+        static bool GetLocString_PrefixPatch(ref string __result)
+        {
+            __result = "Shoutouts to Simpleflips.";
+            return false;
+        }
+    }
+}
---
+++ SampleMods/SampleMod/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SampleMod")]
+[assembly: AssemblyDescription("Sample mod for World Machine Loader")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("hat_kid")]
+[assembly: AssemblyProduct("SampleMod")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6dc58329-aa73-4f55-a4b6-da7f0f772db7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
